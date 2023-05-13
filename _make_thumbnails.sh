53372911ecf14b893c546077da725067afc3bec5
guptad2 <devaansh.gupta@aalto.fi>
2023-05-13 19:42:15 -0400

    update

--- _make_thumbnails.sh
+++ _make_thumbnails.sh
@@ -1,5 +1,5 @@
 # From https://github.com/leonidk/new_website
 mkdir -p tn/publications/images
-mogrify -path tn/publications/images -thumbnail 300x300 publications/images/*.png
-mogrify -path tn/publications/images -thumbnail 300x300 publications/images/*.jpg
-mogrify -path tn/publications/images -thumbnail 300x300 publications/images/*.gif
+mogrify -path tn/publications/images -thumbnail 600x600 publications/images/*.png
+mogrify -path tn/publications/images -thumbnail 600x600 publications/images/*.jpg
+mogrify -path tn/publications/images -thumbnail 600x600 publications/images/*.gif
